ef7261bb6f81f2b1bc2f9e7283bbbfc48d1fd3fb
Pratik5676 <pratikvsalunkhe924@gmail.com>
2025-10-16 14:38:39 +0530

    add animation in the ui using framer motion and frontend completed

--- client/src/components/Testimonial.jsx
+++ client/src/components/Testimonial.jsx
@@ -1,8 +1,15 @@
 import { assets, testimonialsData } from "../assets/assets"
+import { motion } from "motion/react"
 
 const Testimonial = () => {
     return (
-        <div className="flex flex-col items-center justify-center my-20 py-12">
+        <motion.div
+            className="flex flex-col items-center justify-center my-20 py-12"
+            initial={{ opacity: 0.2, y: 100 }}
+            transition={{ duration: 1 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            viewport={{ once: true }}
+        >
             <h1 className="text-3xl sm:text-4xl font-semibold mb-2">Customer testimonials</h1>
 
             <p className="text-gray-500 mb-12">What our users are saying</p>
@@ -32,7 +39,7 @@ const Testimonial = () => {
                     ))
                 }
             </div>
-        </div>
+        </motion.div>
     )
 }
 
