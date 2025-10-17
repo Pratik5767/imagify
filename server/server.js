1bb8b6482ffe7fd69c3c624b87f1ba97f0315543
Pratik5676 <pratikvsalunkhe924@gmail.com>
2025-10-17 15:38:57 +0530

    generate image api completed

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import "dotenv/config";
 import connectDB from "./config/db.js";
 import userRouter from "./routes/userRoutes.js";
+import imageRouter from "./routes/imageRoutes.js";
 
 const PORT = process.env.PORT || 4000;
 const app = express();
@@ -13,7 +14,7 @@ app.use(cors());
 connectDB();
 
 app.use('/api/user', userRouter);
-
+app.use('/api/image', imageRouter);
 app.get('/', (req, res) => res.send('Api working'));
 
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
